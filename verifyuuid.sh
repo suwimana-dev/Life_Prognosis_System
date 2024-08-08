9519d6128de8a05f445c38517dca25f1980426e4
tnabongo <tnabongo@andrew.cmu.edu>
2024-08-08 14:10:30 +0200

    Made changes on registration

--- verifyuuid.sh
+++ verifyuuid.sh
@@ -2,7 +2,7 @@
 
 uuid=$1  # First argument: UUID to verify
 # Use grep to search for the UUID in uuid.txt and cut to extract the email part
-email=$(grep "$uuid" user-store.txt | cut -d '  ' -f1)
+email=$(grep "$uuid" user-store.txt | cut -d ';' -f1)
 if [ -z "$email" ]; then  # Check if the email variable is empty (UUID not found)
     echo "Invalid UUID."  # Print error message
 else
